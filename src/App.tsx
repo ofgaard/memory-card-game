0dcf64302b56569009fadaef790497d48ce127fc
Oliver <oliverfruergaard@gmail.com>
2024-11-01 16:01:20 +0100

    Make scoreboard logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ function App() {
 
 
   return (
-    <div className='flex flex-col items-center h-screen bg-neutral-800 p-10'>
+    <div className='flex flex-col items-center h-screen bg-neutral-800 p-10 gap-5'>
+      <h2 className='text-2xl sm:text-4xl text-neutral-200 font-bold'>The Tony Soprano Memory Game</h2>
 
      <Gameboard/>
 
     </div>
   )
 }
+
 
 export default App
--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -4,32 +4,52 @@ import useGifs from "../hooks/useGifs";
 export default function Gameboard() {
     const [clickedGifs, setClickedGifs] = useState([]);
     const { gifs, shuffleGifs } = useGifs();
+    const [highScore, setHighScore] = useState(0);
 
+    let score = clickedGifs.length;
 
     const handleTurn = (clickedGif) => {
         const isClicked = clickedGifs.some(gif => gif.id === clickedGif.id)
         if (!isClicked) {
             setClickedGifs([ ... clickedGifs, clickedGif ])
         }
+        else if (isClicked) {
+            setClickedGifs([])
+            if (score > highScore) {
+                setHighScore(score)
+            };
+        };
+
         shuffleGifs();
+        increaseHighScore();
         console.log(`you clicked GIF: ${clickedGif.id}`)
 
     }
 
+
+    
+   
+
+
     return (
-        
+        <div className="flex flex-col gap-5">
+            <div className="mx-auto flex gap-5">
+          <h1 className="text-white">Score is: {score}</h1>
+          <h1 className="text-white"> High Score is: {highScore}</h1>
+          </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-5">
 
                 {gifs.map((gif) => (
                    
-                        <div className="w-full h-72 flex flex-col rounded-md p-1 items-center text-neutral-200 bg-slate-700" key={gif.id}>
+                        <div className="w-full h-72 flex flex-col rounded-md p-1 items-center text-neutral-200 bg-slate-700 hover:scale-105 transition-transform duration-300 " key={gif.id}>
                         <img src={gif.images.fixed_height.url} onClick={() => handleTurn(gif)} className="object-cover w-full h-[90%]"/>
-                        <h1>Soprano</h1>
                         </div>
                        
                  ))}
+                                       
             </div>
 
+</div>
     )
 }
 
