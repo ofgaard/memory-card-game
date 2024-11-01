26c5a415e1e89ee784bb46ece4b0093c6d5080fe
Oliver <oliverfruergaard@gmail.com>
2024-11-01 16:03:57 +0100

    add rules

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className='flex flex-col items-center h-screen bg-neutral-800 p-10 gap-5'>
       <h2 className='text-2xl sm:text-4xl text-neutral-200 font-bold'>The Tony Soprano Memory Game</h2>
+      <p className='text-white'>Get points by clicking the images – but don't click one image more than once!</p>
 
      <Gameboard/>
 
